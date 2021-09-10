0ffe2ae6fe0dddca9b7d5b4f4794efb06d6ed9b0
Facu Tissera <facundo8673@gmail.com>
2021-09-10 17:21:27 -0300

    Agregar archivos de proyecto.

---
+++ Negocio/HistorialBug.cs
@@ -0,0 +1,43 @@
+﻿using AppBTS.Datos;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBTS.Negocio
+{
+    class HistorialBug
+    {
+        public int IdBugHistorico { get; set; }
+        public DateTime FechaHistorico { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public int IdBug { get; set; }
+        public int IdResponsable { get; set; }
+        public int IdAsignado { get; set; }
+        public int IdProducto { get; set; }
+        public int IdPrioridad { get; set; }
+        public int IdCriticidad { get; set; }
+        public int IdEstado { get; set; }
+        public bool Borrado { get; set; }
+
+        public DataTable RecuperarHistorialBug(int idBug)
+        {
+            string consulta = "SELECT h.fecha_historico, u.usuario as responsable, e.nombre as estado, us.usuario as asignado"
+                               + " FROM (((BugsHistorico h "
+                               + " LEFT JOIN Usuarios u on u.id_usuario = h.id_usuario_responsable)"
+                               + " JOIN Estados e on e.id_estado = h.id_estado)"
+                               + " LEFT JOIN Usuarios us on h.id_usuario_asignado = us.id_usuario)"
+                               + " WHERE h.id_bug ="+ idBug;
+                              
+
+            BDHelper oDatos = new BDHelper();
+            return oDatos.consultar(consulta);
+        }
+
+
+
+    }
+}
---
+++ Presentacion/frmDetalleBug.cs
@@ -0,0 +1,73 @@
+﻿using AppBTS.Negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace AppBTS.Presentacion
+{
+    public partial class frmDetalleBug : Form
+    {
+        private int idBug;
+
+        public int IdBug { get => idBug; set => idBug = value; }
+
+        public frmDetalleBug()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDetalleBug_Load(object sender, EventArgs e)
+        {
+
+            actualizarCampos();
+           
+
+        }
+
+        private void actualizarCampos()
+        {
+            DataTable tabla = new DataTable();
+
+            Bug oBugSeleccionado = new Bug();
+            tabla = oBugSeleccionado.RecuperarUno(idBug);
+
+            txt_nro_bug.Text = tabla.Rows[0]["id_bug"].ToString();
+            txt_titulo.Text = tabla.Rows[0]["titulo"].ToString();
+            txt_producto.Text = tabla.Rows[0]["producto"].ToString();
+            txt_fec_alta.Text = tabla.Rows[0]["fecha_alta"].ToString();
+            txt_descri.Text = tabla.Rows[0]["descripcion"].ToString();
+            txt_prioridad.Text = tabla.Rows[0]["prioridad"].ToString();
+            txt_criticidad.Text = tabla.Rows[0]["criticidad"].ToString();
+
+            tabla.Clear();
+            HistorialBug oHistorialBug = new HistorialBug();
+            tabla = oHistorialBug.RecuperarHistorialBug(idBug);
+            if (tabla.Rows.Count>0)
+            {
+                for (int i = 0; i < tabla.Rows.Count; i++)
+                {
+                    dgv_historiales.Rows.Add(tabla.Rows[i]["fecha_historico"],
+                                             tabla.Rows[i]["responsable"],
+                                             tabla.Rows[i]["estado"],
+                                             tabla.Rows[i]["asignado"]);
+                                            
+                }
+            }
+        }
+
+
+        private void btn_aceptar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
